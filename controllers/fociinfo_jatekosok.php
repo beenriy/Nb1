8d533dd8ac37278c020a83d73447569f6e798eb7
beenriy <paloczibernadett@gmail.com>
2023-11-20 19:57:44 +0100

    Commit 5
    Hírek oldal textarea-val,  hírek vezérlő elkészítése

---
+++ controllers/fociinfo_jatekosok.php
@@ -0,0 +1,25 @@
+<?php
+
+class Fociinfo_jatekosok_Controller
+{
+	public $baseName = 'fociinfo_jatekosok';  //meghatározni, hogy melyik oldalon vagyunk
+	public function main(array $vars) // a router által továbbított paramétereket kapja
+	{
+		$beleptetModel = new Jatekosok_Model;  //az osztályhoz tartozó modell
+		//a modellben belépteti a felhasználót
+		$retData = $beleptetModel->get_data($vars);
+
+
+
+		//betöltjük a nézetet
+		$view = new View_Loader($this->baseName."_main");
+
+		//átadjuk a lekérdezett adatokat a nézetnek
+		foreach($retData as $name => $value)
+			$view->assign($name, $value);
+	}
+}
+
+?>
+
+
---
+++ views/hirek_main.php
@@ -0,0 +1,15 @@
+<h2>Hírek, vélemények:</h2>
+<form action="<?= SITE_ROOT ?>hirek" method="post">
+	
+	
+	<textarea name="velemeny" id="velemeny" size="255">
+	</textarea>
+    
+    <input type="submit" value="Küldés">
+</form>
+
+
+
+
+
+
